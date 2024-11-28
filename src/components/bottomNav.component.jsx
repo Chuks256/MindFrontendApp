4edc0f2ab4488d08b49a8ef86f124ce647970d4b
chuks <alexanderchuks310@gmail.com>
2024-11-28 20:23:44 +0100

    working

--- src/components/bottomNav.component.jsx
+++ src/components/bottomNav.component.jsx
@@ -7,8 +7,6 @@ import { BsFillBookmarkDashFill } from "react-icons/bs";
 import { RiSearchFill } from "react-icons/ri";
 import { RiNotification4Fill } from "react-icons/ri";
 
-
-
 const ParentContainer=styled.div`
 position:sticky;
 bottom:0px;
@@ -20,22 +18,32 @@ margin-top:20px;
 
 const BottomNavigationParentContainer =styled.div`
 z-index:1;
-padding:35px;
+padding:20px;
 justify-content:center;
 align-items:center;
 background:grey;
 width:350px;
-background:var(--global-bg-color);
-backdrop-filter:blur(20px);
+background:var(--global-blur-color);
+backdrop-filter:blur(10px);
 `;
 
-
+const NavIconParentContainer=styled.div`
+display:flex;
+color:grey;
+align-items:center;
+justify-content:space-between;
+`;
 
 const BottomNavComponent=()=>{
     return(
         <ParentContainer>
             <BottomNavigationParentContainer>
-
+                <NavIconParentContainer>
+                <RiHomeSmile2Fill style={{cursor:'pointer'}} size="25" />
+                <RiSearchFill style={{cursor:'pointer'}} size="25" />
+                <BsFillBookmarkDashFill  style={{cursor:'pointer'}} size="23" />
+                <RiNotification4Fill style={{cursor:'pointer'}} size="25" />
+                </NavIconParentContainer>
             </BottomNavigationParentContainer>
         </ParentContainer>
     )
